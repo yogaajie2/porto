ee66a44837d3c401a0816fd53efc378012bcf719
yogaajie2 <yogaajie2@gmail.com>
2021-10-27 10:03:48 +0700

    revert(server config): re-enable local IP host

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,8 +51,8 @@ export default {
   //   middleware: 'delay'
   // },
 
-  // server: {     
-  //   port: 8000, // default: 3000     
-  //   host: '0.0.0.0', // default: localhost   
-  // }
+  server: {     
+    port: 8000, // default: 3000     
+    host: '0.0.0.0', // default: localhost   
+  }
 }
