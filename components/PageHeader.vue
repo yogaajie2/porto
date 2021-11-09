dc64d284cd2f5cc9356b4100c0bd648eca0e516b
yogaajie2 <yogaajie2@gmail.com>
2021-11-09 10:19:42 +0700

    fix(PageHeader): Add placeholder before load, to prevent first project coming up into viewport

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -1,31 +1,38 @@
 <template>
-  <ZoomIn @after-enter="isSubtitleShown = true; $nuxt.$emit('startHeaderAnimation')">
-    <section
-      class="min-h-screen bg-primary-light pt-20 lg:mt-20"
-      v-show="isContentShown"
-    >
-      <div class="h-screen space-y-8 container mx-auto px-8 py-20 md:px-16 lg:px-24 xl:px-32">
-        <FadeIn @after-enter="isTitleShown = true">
-          <h3
-            class="opacity-0 text-gray-400"
-            v-show="isSubtitleShown"
-          >{{ pageSubtitle }}</h3>
-        </FadeIn>
-        
-        <SlideUp @after-enter="isCTAShown = true">
-          <h1 v-show="isTitleShown">{{ pageTitle }}</h1>
-        </SlideUp>
+  <section>
+    <div
+      v-show="isPlaceholderShown"
+      class="min-h-screen"
+    ></div>
 
-        <transition
-          enter-class="opacity-0 -translate-x-4"
-          enter-active-class="transition transform duration-500"
-          enter-to-class="opacity-100 -translate-x-0"
-        >
-          <CTA v-show="isCTAShown" />
-        </transition>
+    <ZoomIn @after-enter="isSubtitleShown = true; $nuxt.$emit('startHeaderAnimation')">
+      <div
+        class="min-h-screen bg-primary-light pt-20 lg:mt-20"
+        v-show="isContentShown"
+      >
+        <div class="h-screen space-y-8 container mx-auto px-8 py-20 md:px-16 lg:px-24 xl:px-32">
+          <FadeIn @after-enter="isTitleShown = true">
+            <h3
+              class="opacity-0 text-gray-400"
+              v-show="isSubtitleShown"
+            >{{ pageSubtitle }}</h3>
+          </FadeIn>
+          
+          <SlideUp @after-enter="isCTAShown = true">
+            <h1 v-show="isTitleShown">{{ pageTitle }}</h1>
+          </SlideUp>
+  
+          <transition
+            enter-class="opacity-0 -translate-x-4"
+            enter-active-class="transition transform duration-500"
+            enter-to-class="opacity-100 -translate-x-0"
+          >
+            <CTA v-show="isCTAShown" />
+          </transition>
+        </div>
       </div>
-    </section>
-  </ZoomIn>
+    </ZoomIn>
+  </section>
 </template>
 
 <script>
@@ -44,6 +51,7 @@ export default {
 
   data() {
     return {
+      isPlaceholderShown: true,
       isContentShown: false,
       isSubtitleShown: false,
       isTitleShown: false,
@@ -56,7 +64,10 @@ export default {
   },
 
   methods: {
-    showContents() { this.isContentShown = true; }
+    showContents() {
+      this.isPlaceholderShown = false;
+      this.isContentShown = true; 
+    }
   }
 }
 </script>
