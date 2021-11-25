e13961e7e4dd3481bee128f84f09871225a15ad4
yogaajie2 <yogaajie2@gmail.com>
2021-11-25 16:19:18 +0700

    components(CTA): Add  a different CTA for contact page

--- components/CTA.vue
+++ components/CTA.vue
@@ -1,5 +1,20 @@
 <template>
+  <a
+    v-if="$nuxt.$route.path == '/contact'"
+    href="javascript:void(0)"
+    class="items-center inline-flex"
+    @click="$nuxt.$emit('emitScrollToContactForm')"
+  >
+    <p class="cta mr-4 text-tertiary">// Contact Me</p>
+
+    <font-awesome-icon
+      :icon="['fas', 'arrow-down']"
+      class="up-and-down text-tertiary"
+    />
+  </a>
+
   <NuxtLink
+    v-else
     to="/contact"
     @click.native="$nuxt.$emit('showTransition')"
     class="items-center inline-flex"
