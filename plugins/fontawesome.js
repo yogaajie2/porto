806fb17a38a9890618a24478dfbd80c7bf4b303d
yogaajie2 <yogaajie2@gmail.com>
2021-11-25 14:57:53 +0700

    plugins(fontawesome): add faArrowDown icon

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faArrowRight, faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons'
+import { faArrowRight, faArrowDown, faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons'
 
 // This is important, we are going to let Nuxt.js worry about the CSS
 config.autoAddCss = false
 
 // You can add your icons directly in this plugin. See other examples for how you
 // can add other styles or just individual icons.
-library.add(faArrowRight, faAngleDoubleRight)
+library.add(faArrowRight, faArrowDown, faAngleDoubleRight)
 
 // Register the component globally
 Vue.component('font-awesome-icon', FontAwesomeIcon)
