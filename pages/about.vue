b924ab2466f096c3ee2fadd944638e337b6a0c85
yogaajie2 <yogaajie2@gmail.com>
2021-11-26 13:25:20 +0700

    contents(about): Update about page second paragraph

--- pages/about.vue
+++ pages/about.vue
@@ -11,12 +11,8 @@
       
       <div class="space-y-4">
         <p>I am a front-end developer based in the Greater Jakarta Area, Indonesia where I have been working for more than 2 years.</p>
-
-        <p>(Change this) I work from the user experience to the visual details and then transform these in working code. I am a minimal designer and I really like the simple and elegant style. I Worked with a range of clients focusing on web apps and platforms with <b>HCL</b> in partnership with <b>IBM</b>.</p>
-
-        <!-- <p>I am the founder of <b>Persona</b> team a collective of corious people with a particular focus for design and development. My design is also featured in the book Interactive Design for Screen from Sandu Publishing.</p>
-
-        <p>From 2019 I’m <b>Toptal Designer</b> and from 2020 a judge for <b>CSSfox</b></p> -->
+        <p>I have worked on projects both personally and together in teams, and I consider every project as an opportunity to learn, collaborate, and further improve my skills.</p>
+        <!-- Add one more paragraph here -->
       </div>
     </section>
 
