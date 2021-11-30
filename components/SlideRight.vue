e38d182e1541ec21d7b465c5b30063c94cb208ed
yogaajie2 <yogaajie2@gmail.com>
2021-11-30 16:25:47 +0700

    component: Add new SlideRight component

---
+++ components/SlideRight.vue
@@ -0,0 +1,9 @@
+<template>
+  <transition
+    enter-class="opacity-0 -translate-x-4"
+    enter-active-class="transition transform duration-500"
+    enter-to-class="opacity-100 -translate-x-0"
+  >
+    <slot></slot>
+  </transition>
+</template>
