63a5aa90ac209d13e58e9c6f138a6ff544844a0b
yogaajie2 <yogaajie2@gmail.com>
2021-11-30 16:29:19 +0700

    UI/UX(Skills): Add transition group and IntersectionObserver to trigger it

--- components/Skills.vue
+++ components/Skills.vue
@@ -5,26 +5,99 @@
       <h2 class="mb-20 lg:text-5xl xl:text-6xl">I strive to incorporate new & emerging technologies to my tech stack.</h2>
 
       <div class="lg:flex lg:items-end lg:justify-between">
-        <div class="flex justify-between mb-12 md:justify-around md:mb-20 lg:mb-0 lg:space-x-16">
+        <div
+          v-show="isPlaceholderShown"
+          class="h-40"
+        ></div>
+
+        <div
+          v-show="isListShown"
+          class="flex justify-between mb-12 md:justify-around md:mb-20 lg:mb-0 lg:space-x-16"
+        >
           <div>
             <p class="font-bold mb-2 lg:text-2xl">Tools & Languages</p>
-            <p>HTML5 / CSS3</p>
-            <p>JavaScript ES6</p>
-            <p>PHP</p>
-            <p>Git</p>
-            <p>Wordpress</p>
+            
+            <transition-group
+              enter-class="opacity-0 translate-y-4"
+              enter-active-class="transition transform duration-500"
+              enter-to-class="opacity-100 translate-y-0"
+            >
+              <p
+                v-for="language in languages"
+                v-show="isListShown"
+                :key="language"
+              >{{ language }}</p>
+            </transition-group>
           </div>
 
           <div class="md:text-right">
             <p class="font-bold mb-2 lg:text-2xl">Frameworks</p>
-            <p>Vue.js</p>
-            <p>Nuxt.js</p>
-            <p>Laravel</p>
-            <p>Tailwind CSS</p>
-            <p>Bootstrap</p>
+            
+            <transition-group
+              enter-class="opacity-0 translate-y-4"
+              enter-active-class="transition transform duration-500"
+              enter-to-class="opacity-100 translate-y-0"
+            >
+              <p
+                v-for="framework in frameworks"
+                v-show="isListShown"
+                :key="framework"
+              >{{ framework }}</p>
+            </transition-group>
           </div>
         </div>
       </div>
     </div>
   </section>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      isPlaceholderShown: true,
+      isListShown : false,
+
+      languages: [
+        'HTML5 / CSS3',
+        'JavaScript ES6',
+        'PHP',
+        'Git',
+        'Wordpress'
+      ],
+
+      frameworks: [
+        'Vue.js',
+        'Nuxt.js',
+        'Laravel',
+        'Tailwind CSS',
+        'Bootstrap'
+      ],
+
+      observer: null
+    }
+  },
+
+  mounted() {
+    let options = { threshold: 1.0 }
+
+    this.observer = new IntersectionObserver(this.handleObserver, options);
+    this.observer.observe(this.$el);
+  },
+  
+  destroyed() {
+    this.observer.disconnect();
+  },
+
+  methods: {
+    handleObserver(entries) {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          this.isPlaceholderShown = false;
+          this.isListShown = true;
+        }
+      });
+    }
+  }
+}
+</script>
