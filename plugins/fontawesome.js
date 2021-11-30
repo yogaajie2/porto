7cfc3be023cc4fcbd273c64b0d8e689b68e97645
yogaajie2 <yogaajie2@gmail.com>
2021-11-30 16:34:16 +0700

    plugins(fontawesome): Add Instagram, Linkedin, and Github icons

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,14 +1,15 @@
 import Vue from 'vue'
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faArrowRight, faArrowDown, faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons'
+import { faAngleDoubleRight, faArrowRight, faArrowDown } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 // This is important, we are going to let Nuxt.js worry about the CSS
 config.autoAddCss = false
 
 // You can add your icons directly in this plugin. See other examples for how you
 // can add other styles or just individual icons.
-library.add(faArrowRight, faArrowDown, faAngleDoubleRight)
+library.add(faAngleDoubleRight, faArrowRight, faArrowDown, faGithub, faInstagram, faLinkedin)
 
 // Register the component globally
 Vue.component('font-awesome-icon', FontAwesomeIcon)
