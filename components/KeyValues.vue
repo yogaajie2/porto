a0744c421144ae91c7a0f679cac22503873ef6d0
yogaajie2 <yogaajie2@gmail.com>
2021-12-01 12:00:33 +0700

    components(Process): Rename Process component to KeyValues component

---
+++ components/KeyValues.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="bg-primary container mx-auto px-8 py-12 text-secondary md:p-16 lg:px-24 lg:py-20 xl:px-32">
+    <h3 class="mb-4">Key Values</h3>
+    <h2 class="mb-20 lg:text-5xl xl:text-6xl">These are the key values I always follow whenever I write or develop codes</h2>
+
+    <div class="text-center space-y-16 lg:flex lg:flex-wrap lg:space-y-0">
+      <div class="lg:mb-24 lg:w-1/2">
+        <img
+          src="https://via.placeholder.com/60"
+          class="mb-4 mx-auto"
+        />
+        <h5 class="mb-8">Balance</h5>
+        <p class="leading-loose text-gray-500 lg:leading-8 lg:mx-auto lg:text-xl lg:w-4/5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fringilla phasellus faucibus scelerisque eleifend donec.</p>
+      </div>
+
+      <div class="lg:mb-24 lg:w-1/2">
+        <img
+          src="https://via.placeholder.com/60"
+          class="mb-4 mx-auto"
+        />
+        <h5 class="mb-8">Scalability</h5>
+        <p class="leading-loose text-gray-500 lg:leading-8 lg:mx-auto lg:text-xl lg:w-4/5">Neque egestas congue quisque egestas diam in. At ultrices mi tempus imperdiet nulla. Orci eu lobortis elementum nibh tellus molestie nunc non blandit.</p>
+      </div>
+
+      <div class="lg:w-1/2">
+        <img
+          src="https://via.placeholder.com/60"
+          class="mb-4 mx-auto"
+        />
+        <h5 class="mb-8">Compatibility</h5>
+        <p class="leading-loose text-gray-500 lg:leading-8 lg:mx-auto lg:text-xl lg:w-4/5">Sollicitudin tempor id eu nisl nunc mi. Egestas dui id ornare arcu odio ut. Amet mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien.</p>
+      </div>
+
+      <div class="lg:w-1/2">
+        <img
+          src="https://via.placeholder.com/60"
+          class="mb-4 mx-auto"
+        />
+        <h5 class="mb-8">Performance</h5>
+        <p class="leading-loose text-gray-500 lg:leading-8 lg:mx-auto lg:text-xl lg:w-4/5">Dolor magna eget est lorem ipsum dolor sit amet. Sed vulputate mi sit amet mauris commodo. Ut porttitor leo a diam sollicitudin tempor.</p>
+      </div>
+    </div>
+  </section>
+</template>
