e2ab9347f274f08627fed36f9b6034f99546a17b
yogaajie2 <yogaajie2@gmail.com>
2021-12-01 12:01:36 +0700

    UI(TheFooter): Fix TheFooter container and background in lg breakpoint

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -2,99 +2,103 @@
   <footer>
     <section
       v-show="$nuxt.$route.path != '/contact'"
-      class="container mx-auto px-8 pt-12 pb-12 text-secondary bg-primary-lighter md:px-16 lg:px-24 xl:px-32"
+      class="text-secondary bg-primary-lighter "
     >
-      <!-- <div class="space-y-8 md:space-y-24 lg:flex lg:items-center lg:justify-between lg:space-y-0"> -->
-        <div
-          v-show="isPlaceholderShown"
-          class="h-24"
-        ></div>
-
-        <div v-show="isContentShown">
-          <div class="min-h-24 md:text-center lg:text-left lg:space-y-0">
-            <SlideUp @after-enter="isCTAShown = true">
-              <h2
-                v-show="isQuestionShown"
-                class="mb-8"
-              >Have a project?</h2>
-            </SlideUp>
+      <div class="container mx-auto px-8 pt-12 pb-12 md:px-16 lg:px-24 xl:px-32">
+        <!-- <div class="space-y-8 md:space-y-24 lg:flex lg:items-center lg:justify-between lg:space-y-0"> -->
+          <div
+            v-show="isPlaceholderShown"
+            class="h-24"
+          ></div>
   
-            <SlideRight>
-              <CTA v-show="isCTAShown" />
-            </SlideRight>
+          <div v-show="isContentShown">
+            <div class="min-h-24 md:text-center lg:text-left lg:space-y-0">
+              <SlideUp @after-enter="isCTAShown = true">
+                <h2
+                  v-show="isQuestionShown"
+                  class="mb-8"
+                >Have a project?</h2>
+              </SlideUp>
+    
+              <SlideRight>
+                <CTA v-show="isCTAShown" />
+              </SlideRight>
+            </div>
           </div>
-        </div>
-
-        <!-- <div class="flex mb-12 space-x-16 text-lg md:justify-center md:space-x-24 xl:space-x-32">
-          <nav class="flex flex-col space-y-4">
-            <p class="font-bold mb-2">Social</p>
-
-            <a
-              href="https://www.instagram.com/yogaajie2/"
-              target="_blank"
-            >instagram</a>
-
-            <a
-              href="https://www.linkedin.com/in/yogajie"
-              target="_blank"
-            >linkedin</a>
-            
-            <a
-              href="https://github.com/yogaajie2"
-              target="_blank"
-            >github</a>
-          </nav>
-
-          <nav class="flex flex-col space-y-4 md:text-right lg:text-left">
-            <p class="font-bold mb-2">Navigation</p>
-
-            <NuxtLink
-              to="/about"
-              @click.native="$nuxt.$emit('showTransition')"
-            >about</NuxtLink>
-
-            <NuxtLink
-              to="/projects"
-              @click.native="$nuxt.$emit('showTransition')"
-            >projects</NuxtLink>
-
-            <NuxtLink
-              to="/contact"
-              @click.native="$nuxt.$emit('showTransition')"
-            >contact</NuxtLink>
-          </nav>
-        </div>
-      </div> -->
+  
+          <!-- <div class="flex mb-12 space-x-16 text-lg md:justify-center md:space-x-24 xl:space-x-32">
+            <nav class="flex flex-col space-y-4">
+              <p class="font-bold mb-2">Social</p>
+  
+              <a
+                href="https://www.instagram.com/yogaajie2/"
+                target="_blank"
+              >instagram</a>
+  
+              <a
+                href="https://www.linkedin.com/in/yogajie"
+                target="_blank"
+              >linkedin</a>
+              
+              <a
+                href="https://github.com/yogaajie2"
+                target="_blank"
+              >github</a>
+            </nav>
+  
+            <nav class="flex flex-col space-y-4 md:text-right lg:text-left">
+              <p class="font-bold mb-2">Navigation</p>
+  
+              <NuxtLink
+                to="/about"
+                @click.native="$nuxt.$emit('showTransition')"
+              >about</NuxtLink>
+  
+              <NuxtLink
+                to="/projects"
+                @click.native="$nuxt.$emit('showTransition')"
+              >projects</NuxtLink>
+  
+              <NuxtLink
+                to="/contact"
+                @click.native="$nuxt.$emit('showTransition')"
+              >contact</NuxtLink>
+            </nav>
+          </div>
+        </div> -->
+      </div>
     </section>
     
-    <section class="container flex flex-col justify-center mx-auto py-4 space-y-4 text-primary bg-secondary md:px-16 lg:px-24 xl:px-32">
-      <nav class="flex justify-center text-xl">
-        <a
-          href="https://www.instagram.com/yogaajie2/"
-          target="_blank"
-          class="p-4"
-        >
-          <font-awesome-icon :icon="['fab', 'instagram']" />
-        </a>
-
-        <a
-          href="https://www.linkedin.com/in/yogajie"
-          target="_blank"
-          class="p-4"
-        >
-          <font-awesome-icon :icon="['fab', 'linkedin']" />
-        </a>
-        
-        <a
-          href="https://github.com/yogaajie2"
-          target="_blank"
-          class="p-4"
-        >
-          <font-awesome-icon :icon="['fab', 'github']" />
-        </a>
-      </nav>
-
-      <span class="block font-heading text-center">// Carpe Momentum</span>
+    <section class="text-primary bg-secondary md:px-16 lg:px-24 xl:px-32">
+      <div class="container flex flex-col justify-center mx-auto py-4 space-y-4 md:px-16 lg:px-24 xl:px-32">
+        <nav class="flex justify-center text-xl">
+          <a
+            href="https://www.instagram.com/yogaajie2/"
+            target="_blank"
+            class="p-4"
+          >
+            <font-awesome-icon :icon="['fab', 'instagram']" />
+          </a>
+  
+          <a
+            href="https://www.linkedin.com/in/yogajie"
+            target="_blank"
+            class="p-4"
+          >
+            <font-awesome-icon :icon="['fab', 'linkedin']" />
+          </a>
+          
+          <a
+            href="https://github.com/yogaajie2"
+            target="_blank"
+            class="p-4"
+          >
+            <font-awesome-icon :icon="['fab', 'github']" />
+          </a>
+        </nav>
+  
+        <span class="block font-heading text-center">// Carpe Momentum</span>
+      </div>
     </section>
   </footer>
 </template>
