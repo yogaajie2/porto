bb97d40d776461af6175d432ed138f2d29cf58db
yogaajie2 <yogaajie2@gmail.com>
2021-12-13 15:11:54 +0700

    plugins(fontawesome): Add some icons for the KeyValues component

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faAngleDoubleRight, faArrowRight, faArrowDown } from '@fortawesome/free-solid-svg-icons'
+import { faAngleDoubleRight, faArrowRight, faArrowDown, faBalanceScale, faCogs, faShapes, faTachometerAlt } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 // This is important, we are going to let Nuxt.js worry about the CSS
@@ -9,7 +9,7 @@ config.autoAddCss = false
 
 // You can add your icons directly in this plugin. See other examples for how you
 // can add other styles or just individual icons.
-library.add(faAngleDoubleRight, faArrowRight, faArrowDown, faGithub, faInstagram, faLinkedin)
+library.add(faAngleDoubleRight, faArrowRight, faArrowDown, faBalanceScale, faCogs, faShapes, faTachometerAlt, faGithub, faInstagram, faLinkedin)
 
 // Register the component globally
 Vue.component('font-awesome-icon', FontAwesomeIcon)
