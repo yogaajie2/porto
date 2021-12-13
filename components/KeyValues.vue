94bbd9f3bb27068fa371d0661bb27da680a80d6d
yogaajie2 <yogaajie2@gmail.com>
2021-12-13 15:12:51 +0700

    contents(KeyValues): Replace placeholder content

--- components/KeyValues.vue
+++ components/KeyValues.vue
@@ -1,43 +1,52 @@
 <template>
   <section class="bg-primary container mx-auto px-8 py-12 text-secondary md:p-16 lg:px-24 lg:py-20 xl:px-32">
     <h3 class="mb-4">Key Values</h3>
-    <h2 class="mb-20 lg:text-5xl xl:text-6xl">These are the key values I always follow whenever I write or develop codes</h2>
+    <h2 class="mb-20 lg:text-5xl xl:text-6xl">These are the key values I follow when I write or develop codes</h2>
 
     <div class="text-center space-y-16 lg:flex lg:flex-wrap lg:space-y-0">
       <div class="lg:mb-24 lg:w-1/2">
-        <img
-          src="https://via.placeholder.com/60"
+        <font-awesome-icon
+          :icon="['fas', 'balance-scale']"
+          size="3x"
           class="mb-4 mx-auto"
         />
+
         <h5 class="mb-8">Balance</h5>
-        <p class="leading-loose text-gray-500 lg:leading-8 lg:mx-auto lg:text-xl lg:w-4/5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fringilla phasellus faucibus scelerisque eleifend donec.</p>
+        <p class="leading-loose text-gray-500 lg:leading-8 lg:mx-auto lg:text-xl lg:w-4/5">I always try to strike the ideal balance between design and functionality. Which requires me to precisely bridge the gap between designers and back-end developers.</p>
       </div>
 
       <div class="lg:mb-24 lg:w-1/2">
-        <img
-          src="https://via.placeholder.com/60"
+        <font-awesome-icon
+          :icon="['fas', 'shapes']"
+          size="3x"
           class="mb-4 mx-auto"
         />
+
         <h5 class="mb-8">Scalability</h5>
-        <p class="leading-loose text-gray-500 lg:leading-8 lg:mx-auto lg:text-xl lg:w-4/5">Neque egestas congue quisque egestas diam in. At ultrices mi tempus imperdiet nulla. Orci eu lobortis elementum nibh tellus molestie nunc non blandit.</p>
+        <p class="leading-loose text-gray-500 lg:leading-8 lg:mx-auto lg:text-xl lg:w-4/5">I believe code modularity and reusability is the future of web development. I often split my code into reusable components for improved scalability. Making it easier to maintain and faster to implement new features.</p>
+        <!-- <p class="leading-loose text-gray-500 lg:leading-8 lg:mx-auto lg:text-xl lg:w-4/5">Maintaining a massively monolithic code base across teams and developers alike is a slow and complicated process. That's why I believe code modularity and reusability is the future of web development. I often split my code into reusable components for improved scalability. Making it easier to maintain and faster to implement new features.</p> -->
       </div>
 
       <div class="lg:w-1/2">
-        <img
-          src="https://via.placeholder.com/60"
+        <font-awesome-icon
+          :icon="['fas', 'cogs']"
+          size="3x"
           class="mb-4 mx-auto"
         />
+
         <h5 class="mb-8">Compatibility</h5>
-        <p class="leading-loose text-gray-500 lg:leading-8 lg:mx-auto lg:text-xl lg:w-4/5">Sollicitudin tempor id eu nisl nunc mi. Egestas dui id ornare arcu odio ut. Amet mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien.</p>
+        <p class="leading-loose text-gray-500 lg:leading-8 lg:mx-auto lg:text-xl lg:w-4/5">Users nowadays use a wide variety of ways to access the web. That's why it is important for my code to run smoothly across different devices and browsers, old and new.</p>
       </div>
 
       <div class="lg:w-1/2">
-        <img
-          src="https://via.placeholder.com/60"
+        <font-awesome-icon
+          :icon="['fas', 'tachometer-alt']"
+          size="3x"
           class="mb-4 mx-auto"
         />
+
         <h5 class="mb-8">Performance</h5>
-        <p class="leading-loose text-gray-500 lg:leading-8 lg:mx-auto lg:text-xl lg:w-4/5">Dolor magna eget est lorem ipsum dolor sit amet. Sed vulputate mi sit amet mauris commodo. Ut porttitor leo a diam sollicitudin tempor.</p>
+        <p class="leading-loose text-gray-500 lg:leading-8 lg:mx-auto lg:text-xl lg:w-4/5">Speed is key when visiting websites. Users expect them to load in seconds. That's why I always try to keep load times fast and file sizes tiny.</p>
       </div>
     </div>
   </section>
