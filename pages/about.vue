b6c46e70512e9cb22e77696834b7d800cac4e95b
yogaajie2 <yogaajie2@gmail.com>
2021-12-14 15:46:49 +0700

    contents(about): Add my own photo

--- pages/about.vue
+++ pages/about.vue
@@ -9,6 +9,11 @@
       <h3>About me</h3>
       <h2 class="mb-12 lg:text-5xl xl:text-6xl">Hello, I am Yoga Prasetya.</h2>
       
+      <img
+        src="https://i.imgur.com/5ioqq3c.png"
+        class="mb-8"
+      />
+      
       <div class="space-y-4">
         <p>I am a front-end developer based in the Greater Jakarta Area, Indonesia where I have been working for more than 2 years.</p>
         <p>I have worked on projects both personally and together in teams, and I consider every project as an opportunity to learn, collaborate, and further improve my skills.</p>
