cbba15d2ec66ceface04a6f5d0bbd9ac99438c36
yogaajie2 <yogaajie2@gmail.com>
2021-12-22 16:15:29 +0700

    components: Add new SectionTitle component

---
+++ components/SectionTitle.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h3 class="mb-4">{{ sectionTitle }}</h3>
+    <h2 class="mb-20">{{ sectionSubtitle }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    sectionTitle: {
+      type: String,
+      required: true
+    },
+
+    sectionSubtitle: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
