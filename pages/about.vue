b3ad7679147a3a2b8fc85721c8bdf71df34cdafa
yogaajie2 <yogaajie2@gmail.com>
2021-12-27 11:31:23 +0700

    UI/UX(about): Add various UI fixes for larger screens

--- pages/about.vue
+++ pages/about.vue
@@ -11,13 +11,13 @@
         sectionSubtitle="Hello, I am Yoga Prasetya"
       />
       
-      <div class="flex flex-col space-y-8 md:space-y-12 lg:flex-row lg:space-x-12 lg:space-y-0 xl:space-x-40">
+      <div class="flex flex-col space-y-8 md:space-y-12 lg:flex-row lg:items-center lg:space-x-12 lg:space-y-0 xl:space-x-40">
         <img
           src="https://i.imgur.com/5ioqq3c.png"
-          class="md:mx-auto md:w-1/2 lg:w-2/5 xl:w-1/3"
+          class="md:mx-auto md:w-1/2 lg:h-2/5 lg:w-2/5 xl:h-1/3 xl:w-1/3"
         />
         
-        <div class="space-y-4 lg:space-y-8 xl:text-lg">
+        <div class="space-y-4 md:mx-auto md:w-10/12 lg:space-y-8 xl:w-5/12 xl:text-lg">
           <p>I am a front-end developer based in the Greater Jakarta Area, Indonesia where I have been working for more than 2 years.</p>
           <p>I have worked on projects both personally and together in teams, and I consider every project as an opportunity to learn, collaborate, and further improve my skills.</p>
           <p>I primarily use <a href="https://vuejs.org/" target="_blank" class="underline transition-colors duration-200 hover:text-tertiary">Vue.js</a> together with <a href="https://nuxtjs.org/" target="_blank" class="underline transition-colors duration-200 hover:text-tertiary">Nuxt.js</a> as the go-to framework. I also prefer using <a href="https://tailwindcss.com/" target="_blank" class="underline transition-colors duration-200 hover:text-tertiary">Tailwind CSS</a> to help maintain my design systems.</p>
