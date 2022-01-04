404201a785adb6b7a125bfcf8d6ecc4ce632fe16
yogaajie2 <yogaajie2@gmail.com>
2022-01-04 17:08:42 +0700

    UI/UX(animations): Added animations for various sections

--- pages/contact.vue
+++ pages/contact.vue
@@ -26,40 +26,81 @@
         action=""
         class="mb-20 space-y-16 lg:mb-32 lg:space-y-32"
       >
-        <div class="grid gap-y-8 md:grid-cols-2 md:gap-x-8 md:gap-y-16 lg:gap-y-32">
-          <input
-            type="text"
-            placeholder="What's your name?"
-            class="bg-primary border-b px-4"
+          <FadeIn @after-enter="isInputEmailShown = true">
+            <input
+              v-if="isInputNameShown"
+              type="text"
+              placeholder="What's your name?"
+              class="bg-primary border-b px-4"
+            />
+
+            <div
+              v-else
+              class="h-7"
+            />
+          </FadeIn>
+
+          <FadeIn @after-enter="isTextAreaShown = true">
+            <input
+              v-if="isInputEmailShown"
+              type="email"
+              placeholder="What's your email?"
+              class="bg-primary border-b px-4"
+            />
+
+            <div
+              v-else
+              class="h-7"
+            />
+          </FadeIn>
+
+          <FadeIn @after-enter="isSendMessageButtonShown = true">
+            <textarea
+              v-if="isTextAreaShown"
+              rows="5"
+              placeholder="Tell me more about the project"
+              class="bg-primary border-b px-4 md:col-span-2"
+            />
+
+            <div
+              v-else
+              class="h-31"
+            />
+          </FadeIn>
+
+        <SlideRight>
+          <button
+            v-if="isSendMessageButtonShown"
+            class="cta group flex md:mx-auto"
+          >
+            <span class="logo-on-hover hidden lg:block">//</span>
+            <span class="shift-right-8 border-b border-tertiary translate-x-0">Send Message</span>
+          </button>
+
+          <div
+            v-else
+            class="h-6 lg:h-7"
           />
-
-          <input
-            type="email"
-            placeholder="What's your email?"
-            class="bg-primary border-b px-4"
-          />
-
-          <textarea
-            rows="5"
-            placeholder="Tell me more about the project"
-            class="bg-primary border-b px-4 md:col-span-2"
-          ></textarea>
-        </div>
-
-        <button class="cta group flex md:mx-auto">
-          <span class="logo-on-hover hidden lg:block">//</span>
-          <span class="shift-right-8 border-b border-tertiary translate-x-0">Send Message</span>
-        </button>
+        </SlideRight>
       </form>
 
       <p class="border-t mb-16 pt-4 md:pt-8 md:text-center lg:mb-8 lg:pt-16">Or click the button below to view my resume</p>
 
-      <a href="https://drive.google.com/file/d/1-EPmr6BzppChbAd5sWvxVvJS6tR8ZymV/view?usp=sharing" target="_blank">
-        <button class="cta group flex md:mx-auto">
-          <span class="logo-on-hover hidden lg:block">//</span>
-          <span class="shift-right-8 border-b border-tertiary translate-x-0">View Resume</span>
-        </button>
-      </a>
+        <a
+          href="https://drive.google.com/file/d/1-EPmr6BzppChbAd5sWvxVvJS6tR8ZymV/view?usp=sharing"
+          target="_blank"
+          class="h-7"
+        >
+          <SlideRight>
+            <button
+              v-if="isViewResumeButtonShown"
+              class="cta group flex md:mx-auto"
+            >
+              <span class="logo-on-hover hidden lg:block">//</span>
+              <span class="shift-right-8 border-b border-tertiary translate-x-0">View Resume</span>
+            </button>
+          </SlideRight>
+        </a>
     </section>
   </main>
 </template>
@@ -67,6 +108,16 @@
 <script>
 export default {
   head: { titleTemplate: '%s | Contact' },
+
+  data() {
+    return{
+      isInputNameShown: false,
+      isInputEmailShown: false,
+      isTextAreaShown: false,
+      isSendMessageButtonShown: false,
+      isViewResumeButtonShown: false
+    }
+  },
 
   mounted() {
     this.$nuxt.$on('emitScrollToContactForm', this.scrollToContactForm);
