43aa1d1637c2a183d8247810348b32a20846c286
yogaajie2 <yogaajie2@gmail.com>
2022-01-04 15:17:07 +0700

    components(KeyValues): Split KeyValues list items to a new component, KeyValue

---
+++ components/KeyValue.vue
@@ -0,0 +1,62 @@
+<template>
+  <IntersectionObserverTarget
+    :threshold="1"
+    class="h-88 md:h-57 lg:h-81 xl:h-76"
+    @on-intersecting="handleIntersecting"
+  >
+    <ZoomIn @after-enter="isPlaceholderShown = false; isNameShown = true;">
+      <font-awesome-icon
+        v-if="isIconShown"
+        :icon="['fas', icon]"
+        size="3x"
+        class="mb-4 mx-auto"
+      />
+    </ZoomIn>
+  
+    <transition
+      enter-class="opacity-0 -translate-y-4"
+      enter-active-class="transition transform duration-500"
+      enter-to-class="opacity-100 -translate-y-0"
+      @after-enter="isDescriptionShown = true"
+    >
+      <h5
+        v-if="isNameShown"
+        class="mb-8 lg:text-3xl xl:text-4xl">{{ name }}
+      </h5>
+    </transition>
+    
+    <FadeIn>
+      <p
+        v-if="isDescriptionShown"
+        class="leading-loose text-gray-500 lg:leading-8 lg:text-lg xl:text-xl">{{ description }}
+      </p>
+    </FadeIn>
+  </IntersectionObserverTarget>
+</template>
+
+<script>
+export default {
+  props: [
+    'icon',
+    'name',
+    'description'
+  ],
+
+  data() {
+    return {
+      isIconShown: false,
+      isNameShown: false,
+      isDescriptionShown: false
+    }
+  },
+
+  methods: {
+    handleIntersecting(entry, unobserve) {
+      if (entry.isIntersecting) {
+        this.isIconShown = true;
+        unobserve();
+      }
+    }
+  }
+}
+</script>
--- components/KeyValues.vue
+++ components/KeyValues.vue
@@ -6,50 +6,48 @@
     />
 
     <div class="grid text-center gap-16 lg:grid-cols-2 lg:gap-24">
-      <div>
-        <font-awesome-icon
-          :icon="['fas', 'balance-scale']"
-          size="3x"
-          class="mb-4 mx-auto"
-        />
-
-        <h5 class="mb-8 lg:text-3xl xl:text-4xl">Balance</h5>
-        <p class="leading-loose text-gray-500 lg:leading-8 lg:text-lg xl:text-xl">I always try to strike the ideal balance between design and functionality. Which requires me to precisely bridge the gap between designers and back-end developers.</p>
-      </div>
-
-      <div>
-        <font-awesome-icon
-          :icon="['fas', 'shapes']"
-          size="3x"
-          class="mb-4 mx-auto"
-        />
-
-        <h5 class="mb-8 lg:text-3xl xl:text-4xl">Scalability</h5>
-        <p class="leading-loose text-gray-500 lg:leading-8 lg:text-lg xl:text-xl">I believe code modularity and reusability is the future of web development. I often split my code into reusable components for improved scalability. Making it easier to maintain and faster to implement new features.</p>
-        <!-- <p class="leading-loose text-gray-500 lg:leading-8 lg:text-lg xl:text-xl">Maintaining a massively monolithic code base across teams and developers alike is a slow and complicated process. That's why I believe code modularity and reusability is the future of web development. I often split my code into reusable components for improved scalability. Making it easier to maintain and faster to implement new features.</p> -->
-      </div>
-
-      <div>
-        <font-awesome-icon
-          :icon="['fas', 'cogs']"
-          size="3x"
-          class="mb-4 mx-auto"
-        />
-
-        <h5 class="mb-8 lg:text-3xl xl:text-4xl">Compatibility</h5>
-        <p class="leading-loose text-gray-500 lg:leading-8 lg:text-lg xl:text-xl">Users nowadays use a wide variety of ways to access the web. That's why it is important for my code to run smoothly across different devices and browsers, old and new.</p>
-      </div>
-
-      <div>
-        <font-awesome-icon
-          :icon="['fas', 'tachometer-alt']"
-          size="3x"
-          class="mb-4 mx-auto"
-        />
-
-        <h5 class="mb-8 lg:text-3xl xl:text-4xl">Performance</h5>
-        <p class="leading-loose text-gray-500 lg:leading-8 lg:text-lg xl:text-xl">Speed is key when visiting websites. Users expect them to load in seconds. That's why I always try to keep load times fast and file sizes tiny.</p>
-      </div>
+      <KeyValue
+        v-for="(value, index) in values"
+        :key="index"
+        :icon="value.icon"
+        :name="value.name"
+        :description="value.description"
+      />
     </div>
   </section>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      values: [
+        {
+          icon: 'balance-scale',
+          name: 'Balance',
+          description: 'I always try to strike the ideal balance between design and functionality. Which requires me to precisely bridge the gap between designers and back-end developers.'
+        },
+
+        {
+          icon: 'shapes',
+          name: 'Scalability',
+          description: 'I believe code modularity and reusability is the future of web development. I often split my code into reusable components for improved scalability. Making it easier to maintain and faster to implement new features.'
+          // Maintaining a massively monolithic code base across teams and developers alike is a slow and complicated process. That's why I believe code modularity and reusability is the future of web development. I often split my code into reusable components for improved scalability. Making it easier to maintain and faster to implement new features.
+        },
+
+        {
+          icon: 'cogs',
+          name: 'Compatibility',
+          description: 'Users nowadays use a wide variety of ways to access the web. That\'s why it is important for my code to run smoothly across different devices and browsers, old and new.'
+        },
+
+        {
+          icon: 'tachometer-alt',
+          name: 'Performance',
+          description: 'Speed is key when visiting websites. Users expect them to load in seconds. Which is the reason why I always try to keep load times fast and file sizes tiny.'
+        },
+      ]
+    }
+  }
+}
+</script>
