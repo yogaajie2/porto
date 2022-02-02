2443a836c6f73dd4c9e81bbbf51169f4536184fc
yogaajie2 <yogaajie2@gmail.com>
2022-02-02 16:50:35 +0700

    test: add my first test

---
+++ cypress/integration/sample.spec.js
@@ -0,0 +1,16 @@
+describe('My First Test', function() {
+  it('Gets, types, and asserts', function() {
+    cy.visit('https://example.cypress.io')
+
+    cy.pause()
+
+    cy.contains('type').click()
+
+    cy.url()
+      .should('include', '/commands/actions')
+
+    cy.get('.action-email')
+      .type('fake@email.com')
+      .should('have.value', 'fake@email.com')
+  })
+})
