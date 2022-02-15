06920d1cb6349e9660eeed7efcd960f2b3eb8259
yogaajie2 <yogaajie2@gmail.com>
2022-02-15 14:15:19 +0700

    test: test file for first page load

---
+++ cypress/integration/first-load.spec.js
@@ -0,0 +1,40 @@
+describe('First Load', () => {
+  it('Runs all animations on first load properly and in order', function() {
+    cy.visit('/')
+
+    cy.contains('//').should('have.class', 'animate-fade-in-out')
+
+    cy.get('[data-cy="page-header"]')
+      .should('have.class', 'opacity-0')
+      .and('have.class', 'scale-75')
+      .and('have.class', 'duration-500')
+    
+    cy.get('[data-cy="page-header"]')
+      .should('have.class', 'opacity-100')
+      .and('have.class', 'scale-100')
+      .and('have.class', 'duration-500')
+
+    cy.get('[data-cy="page-subtitle"]')
+      .should('have.class', 'opacity-100')
+      .and('have.class', 'duration-300')
+
+    cy.get('header')
+      .should('have.class', '-translate-y-0')
+      .and('have.class', 'duration-300')
+      .and('have.class', 'ease-in-out')
+
+    cy.get('[data-cy="page-title"]')
+      .should('have.class', 'opacity-100')
+      .and('have.class', 'translate-y-0')
+      .and('have.class', 'duration-500')
+
+    cy.get('[data-cy="logo"]')
+      .should('have.class', 'opacity-100')
+      .and('have.class', 'duration-300')
+
+    cy.get('[data-cy="brand"]')
+      .should('have.class', 'opacity-100')
+      .and('have.class', '-translate-x-0')
+      .and('have.class', 'duration-500')
+  })
+})
